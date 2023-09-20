7b35ed75d506bdcf170e28d857c9e157af2c953a
AbderrahmanKro <elhairechabderrahman@gmail.com>
2023-09-20 08:51:14 +0100

    made the bot into a commands bot

--- main.rb
+++ main.rb
@@ -3,10 +3,12 @@ require 'dotenv/load'
 
 token = ENV['DISCORD_TOKEN']
 
-bot = Discordrb::Bot.new token: token
+bot = Discordrb::Commands::CommandBot.new token: token, client_id: 1153146227152654437, prefix: '!'
 
-bot.message(with_text: "Hey Bot!") do |event|
-  event.respond "Hi, #{event.user.name}!"
+bot.set_user_permission(274625354788896778, 1)
+
+bot.command :random do |event, min, max|
+  rand(min.to_i .. max.to_i)
 end
 
 bot.run
